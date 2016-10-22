820536dbc992a60a3247bcb1d171f98a2702afde
Tianhe <tianhelin@hotmail.com>
2016-10-22 14:26:22 +0000

    Create Model Discusstion & Finish index & new discussion function

---
+++ app/controllers/discussions_controller.rb
@@ -0,0 +1,36 @@
+class DiscussionsController < ApplicationController
+    before_action :set_discusstion, :only => [:show,:edit,:update,:destroy]
+    def index
+        @discussions = Discussion.all
+    end
+    
+    def new
+        @discussion = Discussion.new
+    end
+    
+    def create
+        @discussion = Discussion.new(discussion_params)
+        @discussion.save
+        
+        redirect_to discussions_path
+    end
+    
+    def show
+        
+    end
+    
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+    
+    
+private
+
+    def discussion_params
+      params.require(:discussion).permit(:topic, :article)
+    end
+end
