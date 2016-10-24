5a7382c3054c84004ca0c3554f468a974ee98bc4
Tianhe <tianhelin@hotmail.com>
2016-10-24 08:01:09 +0000

    generate new model 'comment' & controller 'comments'

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,7 @@
 class Discussion < ActiveRecord::Base
    validates :topic,  presence: true
    belongs_to :user, foreign_key: :user_id
+   has_many :comments
    
    def ownerornot?(user)
       user && user.id == user_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024062656) do
+ActiveRecord::Schema.define(version: 20161024075340) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "discussions", force: :cascade do |t|
     t.string   "topic"
